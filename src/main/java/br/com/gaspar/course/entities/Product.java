fa074268f75faf6c512a63917ce26dd9529ebd7f
GasparPSousa <gasparufrj@gmail.com>
2021-11-27 15:18:38 -0300

    Criando todas as classes referente a entidade Product

--- src/main/java/br/com/gaspar/course/entities/Product.java
+++ src/main/java/br/com/gaspar/course/entities/Product.java
@@ -1,6 +1,7 @@
 package br.com.gaspar.course.entities;
 
 
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,29 +16,35 @@ import java.util.Set;
 @Getter
 @Setter
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
 
     @Transient
-    private Set<Product> products = new HashSet<>();
+    private Set<Category> categories = new HashSet<>();
 
-    public Category(Long id, String name) {
+    public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
         this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Category category = (Category) o;
-        return id.equals(category.id);
+        Product product = (Product) o;
+        return id.equals(product.id);
     }
 
     @Override
