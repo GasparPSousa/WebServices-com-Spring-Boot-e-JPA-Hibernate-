5b3272771dcdff346e957b916e166129dbafa4d1
GasparPSousa <gasparufrj@gmail.com>
2021-11-27 13:00:31 -0300

    Order, Instant, ISO 8601 - Parte02

--- src/main/java/br/com/gaspar/course/entities/Order.java
+++ src/main/java/br/com/gaspar/course/entities/Order.java
@@ -1,5 +1,7 @@
 package br.com.gaspar.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,7 +24,10 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
 
     @ManyToOne
     @JoinColumn(name = "client_id")
---
+++ src/main/java/br/com/gaspar/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package br.com.gaspar.course.repositories;
+
+import br.com.gaspar.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
