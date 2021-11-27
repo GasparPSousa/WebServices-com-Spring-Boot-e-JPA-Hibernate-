281a1cf9e24c008d1170c864cde4524b509ffa6e
GasparPSousa <gasparufrj@gmail.com>
2021-11-27 11:30:53 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/br/com/gaspar/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.gaspar.course.repositories;
+
+import br.com.gaspar.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
